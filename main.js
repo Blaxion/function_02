a17a47307334ac5eb7231d8d7e7527a81798a789
Blaxion <khibaziev.f@gmail.com>
2021-04-28 10:37:20 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// Exo 1
+// function reverseNumber(int) {
+//     int = int.toString()
+//     let reversedInt ="";  
+//     for (let i = 0; i < int.length  ; i++) {
+//         reversedInt = int.charAt(i) + reversedInt
+//     }
+//     return parseInt(reversedInt, 10)
+// }
+// console.log(reverseNumber(123456789))
+
+
+// Exo 1
+// function reverseNumber(int) {
+//     return  parseInt(int.toString().split("").reverse().join(""));
+// }
+// console.log(reverseNumber(0123))
+
+
+// // Exo 2
+// function divPar2(int) {
+//     switch (int%2) {
+//         case 0:
+//             alert(`"Le num ${int} est divisible par 2  ${int}:2 = ${int/2}"`);
+//             break;
+//         default:
+//             alert(`Ce nombre n'est pas divisible`)
+//         break;
+//     }
+// }
+// console.log(divPar2(35246468))
+
+
+// // Exo 3
+// function logIn(){
+//     let mdp;
+//     while (mdp != "mdp") {
+//         mdp = prompt("Votre mot de passe")
+//     }
+//     alert("Bravo t'es CO maggle sisisisi")
+// }
+// logIn()
+
